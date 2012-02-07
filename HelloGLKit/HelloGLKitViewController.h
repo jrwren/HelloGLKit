a170e083342e1905b43e027297b1f047afe7146b
Jay R. Wren <jrwren@xmtp.net>
2012-02-07 13:46:30 -0500

    init example

---
+++ HelloGLKit/HelloGLKitViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloGLKitViewController.h
+//  HelloGLKit
+//
+//  Created by Ray Wenderlich on 9/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface HelloGLKitViewController : GLKViewController
+
+@end
---
+++ README.md
@@ -0,0 +1,5 @@
+# opengl es example and tutorial
+
+http://www.raywenderlich.com/5235/beginning-opengl-es-2-0-with-glkit-part-2
+
+I found the tutorial a tiny bit hard to follow and I want to web browse the source to compare oto my source.
